f827f3ec457224ae9e3e98b1e48eccdebfc03e2b
leo <hippochin@gmail.com>
2016-01-30 12:37:59 -0800

    first test

---
+++ Assets/Scripts/test1.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        InputListener.inputHeldEvent += handleInputHeld;
+        GameObject star = Instantiate(Resources.Load("star")) as GameObject;
+        star.transform.SetParent(this.transform);
+        star.transform.position = Vector3.zero;
+	}
+
+    private void handleInputHeld(KeyCode key)
+    {
+        if (key == KeyCode.W)
+            HologramRotate("up");
+    }
+
+    private void HologramRotate(System.String)
+    {
+    }
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
