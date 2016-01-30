545b82c414e8d0bb511457e1df9c4730588c48fa
Warren <warrussell@gmail.com>
2016-01-30 01:32:06 -0800

    Initial commit

---
+++ Assets/Scripts/Screens/GameScreen.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameScreen : MonoBehaviour 
+{	
+	public enum AnimationType{None, Pop, PopAndFade, Fade, SlideFromLeft, SlideFromRight, SlideFromTop, SlideFromBottom}
+	
+	public ScreenManager.ScreenType type { get; protected set; }
+	public ScreenManager.ScreenType source { get; protected set; }
+	
+	private RectTransform _rectTransform;
+	private Canvas _canvas;
+	
+	private AnimationType _animationType;
+
+	private Coroutine _scaleCoroutine;
+	private Coroutine _moveCoroutine;
+	private Coroutine _fadeCoroutine;
+	private Coroutine[] _currentFadingCoroutines;
+	
+	public virtual void Setup(AnimationType animType)
+	{
+		_rectTransform = GetComponent<RectTransform>();
+		_canvas = GetComponent<Canvas>();
+		
+		_animationType = animType;
+		gameObject.SetActive(false);
+	}
+	
+	public void Open()
+	{
+		gameObject.SetActive(true);
+	
+		switch (_animationType)
+		{
+			case AnimationType.None:
+			{
+				break;
+			}
+			case AnimationType.Pop:
+			{
+				Pop();
+				break;
+			}
+			case AnimationType.PopAndFade:
+			{
+				Pop();
+				FadeIn();
+				break;
+			}
+			case AnimationType.Fade:
+			{
+				FadeIn();
+				break;
+			}
+			case AnimationType.SlideFromLeft:
+			{
+				SlideFrom(-1080, 0);
+				break;
+			}
+			case AnimationType.SlideFromRight:
+			{
+				SlideFrom(1080, 0);
+				break;
+			}
+			case AnimationType.SlideFromTop:
+			{
+				SlideFrom(0, 1920);
+				break;
+			}
+			case AnimationType.SlideFromBottom:
+			{
+				SlideFrom(0, -1920);
+				break;
+			}
+		}
+	}
+	
+	public virtual void SetSource(ScreenManager.ScreenType source)
+	{
+		this.source = source;
+	}
+	
+	public virtual void Refresh()
+	{
+	
+	}
+	
+	public virtual void Destroy()
+	{
+		DestroyImmediate(this.gameObject);
+	}
+	
+	public void SetPosition(float x, float y)
+	{
+		_rectTransform.anchoredPosition = new Vector2(x, y);
+	}
+
+	public void Pop()
+	{
+		if (_scaleCoroutine != null) {
+			StopCoroutine(_scaleCoroutine);
+		}
+		
+		if (transform.GetChild(0).name == "Content") {
+			transform.GetChild(0).localScale = Vector3.zero;
+			_scaleCoroutine = StartCoroutine(transform.GetChild(0).ScaleTo(Vector3.one, 0.5f, EaseType.BackOut));
+		}
+	}
+	
+	public void FadeIn()
+	{
+		transform.localScale = Vector3.zero;
+		
+		if (_fadeCoroutine != null) {
+			for (int i = 0; i < _currentFadingCoroutines.Length; ++i)
+			{
+				if (_currentFadingCoroutines[i] != null)
+				{
+					StopCoroutine(_currentFadingCoroutines[i]);
+				}
+			}
+		
+			StopCoroutine(_fadeCoroutine);
+		}
+		
+		_fadeCoroutine = StartCoroutine(ImageUtils.Instance.FadeAllChildrenUI(transform, 0, 1, 0.02f, _currentFadingCoroutines));
+	}
+	
+	
+	
+	public void SlideFrom(float x, float y)
+	{
+		SetPosition(x, y);
+		
+		if (_moveCoroutine != null) {
+			StopCoroutine(_moveCoroutine);
+		}
+		
+		_moveCoroutine = StartCoroutine(_rectTransform.MoveAnchorTo(Vector3.zero, 0.5f, EaseType.SineOut));
+	}	
+}
---
+++ Assets/Scripts/Screens/ScreenManager.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ScreenManager : MonoBehaviour 
+{
+	public enum ScreenType{	
+							Prototype, 
+							Null
+						  }
+
+	/// <summary>
+	/// Gets the instance of ScreenManager.
+	/// </summary>
+	public static ScreenManager Instance
+	{
+		get
+		{
+			if(_instance == null) {
+				_instance = GameObject.FindObjectOfType<ScreenManager>();
+			}
+			
+			return _instance;
+		}
+	}
+	private static ScreenManager _instance;
+	
+	private Stack<GameScreen> _screenLayers;
+	private GameScreen _nextScreen;
+	
+	private void Start()
+	{
+		DontDestroyOnLoad(this.gameObject);
+		_screenLayers = new Stack<GameScreen>();
+	}
+	
+	private void OnApplicationFocus(bool focusStatus)
+	{
+		if (focusStatus) {
+			RefreshScreen();
+		}
+	}
+	
+	public void RefreshScreen()
+	{
+		if (_screenLayers != null && _screenLayers.Count > 0) _screenLayers.Peek().Refresh();
+	}
+	
+	private GameScreen CreateScreen(ScreenType screenType)
+	{
+		GameObject screenPrefab = Instantiate(Resources.Load(screenType.ToString())) as GameObject;
+		return screenPrefab.GetComponent<GameScreen>();
+	}
+	
+	public void PushScreen(ScreenType screenType, GameScreen.AnimationType animType)
+	{
+		ScreenType source = GetCurrentScreenType();
+		PushScreenWithSource(screenType, animType, source);
+	}
+	
+	public void ReplaceScreen(ScreenType screenType, GameScreen.AnimationType animType)
+	{
+		ScreenType source = GetCurrentScreenType();
+		CloseScreen();
+		PushScreenWithSource(screenType, animType, source);
+	}
+	
+	private void PushScreenWithSource(ScreenType screenType, GameScreen.AnimationType animType, ScreenType source)
+	{
+		if (screenType == ScreenType.Null) {
+			throw new System.Exception("Trying to create null screen type");
+		}	
+		
+		GameScreen screen = CreateScreen(screenType);
+		screen.Setup(animType);
+		screen.Open();
+		screen.SetSource(source);
+		_screenLayers.Push(screen);
+	}
+	
+	public void NextScreen(ScreenType screenType, GameScreen.AnimationType animType)
+	{
+		GameScreen screen = CreateScreen(screenType);
+		screen.Setup(animType);
+		_nextScreen = screen;
+	}
+	
+	public void CloseScreen(ScreenType screenType)
+	{
+		if (_screenLayers.Peek().type == screenType) {
+			CloseScreen();
+		}
+	}
+	
+	private void CloseScreen()
+	{
+		if (_screenLayers.Count > 0) {
+			GameScreen screen = _screenLayers.Pop();
+			screen.Destroy();
+			
+			if (_nextScreen != null) {
+				_nextScreen.Open();
+				_screenLayers.Push(_nextScreen);
+				_nextScreen = null;
+			}		
+		}
+	}
+	
+	public ScreenType GetCurrentScreenType()
+	{
+		if (_screenLayers.Count > 0) return _screenLayers.Peek().type;
+		else return ScreenType.Null;
+	}
+}
